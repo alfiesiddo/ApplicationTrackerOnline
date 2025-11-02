c44ce5c23185ab033bc52712a4cac3f48235f801
alfiesiddo <alfiesiddons@gmail.com>
2025-11-02 14:13:51 +0000

    added application data model

---
+++ ApplicationTrackerOnline/Models/JobApplication.cs
@@ -0,0 +1,17 @@
+﻿namespace ApplicationTrackerOnline.Models
+{
+    public class JobApplication
+    {
+        public int Id { get; set; }
+
+        public string Role { get; set; }
+        public string CompanyName { get; set; }
+        public string Location { get; set; }
+        public string PortalURL { get; set; }
+
+
+        public int Status { get; set; } = 0; // 0 = Applied, 1 = Rejected, 2 = Scouted, 3 = Assessments, 4 = Interview, 5 = Offered
+
+        public DateTime AppliedDate { get; set; } = DateTime.Now;
+    }
+}

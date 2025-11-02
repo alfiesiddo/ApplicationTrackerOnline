a1886c164210778cab719fc2ea6b934cd95dac5a
alfiesiddo <alfiesiddons@gmail.com>
2025-11-02 13:54:51 +0000

    Add project files.

---
+++ ApplicationTrackerOnline/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationTrackerOnline.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationTrackerOnline.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}

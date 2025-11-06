f1f863c53d8ae8aa846c1e2e46d1c87240e10532
alfiesiddo <alfiesiddons@gmail.com>
2025-11-06 15:56:00 +0000

    added service class for gathering useful stats to user. added function that returns the most popular locations they applied to too

---
+++ ApplicationTrackerOnline/Services/ApplicationStatsService.cs
@@ -0,0 +1,38 @@
+﻿using ApplicationTrackerOnline.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationTrackerOnline.Services
+{
+    public class ApplicationStatsService
+    {
+        private readonly ApplicationDbContext _context;
+        public ApplicationStatsService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //return top 5 locations user has applied for jobs to.
+        private async Task<List<string>> getTopLocations(string userId)
+        {
+            List<string> output = new List<string>();
+
+            var topLocations = await _context.jobApplications
+                .Where(j => j.UserId == userId && j.Location != null)
+                .GroupBy(j => j.Location)
+                .OrderByDescending(g => g.Count())
+                .Select(g => g.Key)
+                .Take(5)
+                .ToListAsync();
+
+            foreach (var location in topLocations)
+            {
+                output.Add(location.ToString());
+            }
+            return output;
+        }
+
+
+        //show amount of different statuses
+
+    }
+}

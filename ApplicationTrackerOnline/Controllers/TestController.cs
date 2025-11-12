b6e8a56abe173207f05c4f720baffa6f4609a7c2
alfiesiddo <alfiesiddons@gmail.com>
2025-11-12 11:08:38 +0000

    added test controller to test jwt auth

---
+++ ApplicationTrackerOnline/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationTrackerOnline.Controllers.Api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [Authorize(AuthenticationSchemes = "JwtScheme")]
+        [HttpGet("hello")]
+        public IActionResult Hello()
+        {
+            var userId = User?.FindFirst("sub")?.Value;
+            return Ok(new { message = "Hello from protected endpoint!", userId });
+        }
+    }
+}

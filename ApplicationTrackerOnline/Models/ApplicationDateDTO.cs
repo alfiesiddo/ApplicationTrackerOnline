c7149594f4cb0dbd854a0687c2806578884796ad
alfiesiddo <alfiesiddons@gmail.com>
2025-11-27 11:25:32 +0000

    made bare partial card and DTO model for displaying interview dates

---
+++ ApplicationTrackerOnline/Models/ApplicationDateDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace ApplicationTrackerOnline.Models
+{
+    public class ApplicationDateDTO
+    {
+        public DateTime? Date { get; set; }
+        public string Organization {  get; set; }
+
+        public override string ToString()
+        {
+            return $"{Organization}: {Date:dddd/MMMM H:mm tt}";
+        }
+
+    }
+}

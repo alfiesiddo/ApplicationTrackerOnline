3298ca4341c967938f0494d53726985089de634f
alfiesiddo <alfiesiddons@gmail.com>
2025-11-28 16:58:55 +0000

    post endpoint added to add/update new the users downloadable spreadhseet

--- ApplicationTrackerOnline/Controllers/DataController.cs
+++ ApplicationTrackerOnline/Controllers/DataController.cs
@@ -1,7 +1,11 @@
 ﻿using ApplicationTrackerOnline.Data;
+using ApplicationTrackerOnline.Models;
 using ApplicationTrackerOnline.Services;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
 
 namespace ApplicationTrackerOnline.Controllers
 {
@@ -10,12 +14,31 @@ namespace ApplicationTrackerOnline.Controllers
     public class DataController : ControllerBase
     {
         private readonly DataTransferringService _dataService;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
 
-        public DataController(DataTransferringService dataService)
+        public DataController(DataTransferringService dataService, UserManager<ApplicationUser> userManager, ApplicationDbContext context)
         {
             _dataService = dataService;
+            _context = context;
         }
 
+
+        [HttpPatch]
+        public async Task<IActionResult> CreateSheet()
+        {
+            var userId = _userManager.GetUserId(User);
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Unauthorized();
+            }
+
+            List<JobApplication> apps = await _context.jobApplications.Where(a => a.UserId == userId).ToListAsync();
+
+            await _dataService.CreateAndStoreSpreadsheet(apps, userId);
+
+            return Ok("Spreadsheet succesfully created.");
+        }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> Download(int id)

2ab169eb94880a178e84bd455136df93b93539b7
alfiesiddo <alfiesiddons@gmail.com>
2025-11-28 15:51:14 +0000

    added object for feature allowing users to install spreadhseet data of their application profile

---
+++ ApplicationTrackerOnline/Models/ApplicationsSpreadsheet.cs
@@ -0,0 +1,12 @@
+﻿namespace ApplicationTrackerOnline.Models
+{
+    public class ApplicationsSpreadsheet
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+
+        public string Filename { get; set; }
+        public byte[] SheetData { get; set; }
+
+    }
+}
